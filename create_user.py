eb58db0ac5aef3576af1ab927cf7fc03cd644067
devopstrainingschool <107158398+devopstrainingschool@users.noreply.github.com>
2023-01-21 21:31:34 -0600

    Update create_user.py

--- create_user.py
+++ create_user.py
@@ -1,10 +1,12 @@
 import subprocess
 
-def add_user(username, password):
+def add_user():
+    username = input("Enter a username: ")
+    password = input("Enter a password: ")
     try:
         subprocess.run(["useradd", "-m", "-s", "/bin/bash", "-p", password, username], check=True)
     except subprocess.CalledProcessError as e:
         print("Failed to create user:", e)
 
 if __name__ == "__main__":
-    add_user("example", "password")
+    add_user()
